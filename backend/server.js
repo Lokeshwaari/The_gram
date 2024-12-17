adfbc8c74899e43bdd1aac41e9fcdd1ff1ef391d
Lokeshwaari <lokeshwarisai@gmail.com>
2024-12-17 20:54:50 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+//const express = require ("express")
+import express from "express";
+import dotenv from "dotenv";
+import authRoute from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js"
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+import postRoute from "./routes/post.route.js";
+import notificationRoute from "./routes/notification.route.js";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_SECRET_KEY,
+    api_secret : process.env.CLOUDINARY_API_KEY
+})
+
+app.use(cors({
+    origin : "http://localhost:3000",
+    credentials: true 
+}))
+
+
+app.get("/" , (req, res) => {
+    res.send("Hello World!!");
+})
+
+app.use(express.json({
+    limit : "5mb"
+  
+}));
+
+app.use(cookieParser());
+app.use(express.urlencoded({
+    extended :true
+}))
+
+
+
+app.use("/api/auth" , authRoute);
+app.use("/api/users", userRoutes);
+app.use("/api/posts" , postRoute);
+app.use("/api/notifications" , notificationRoute);
+
+app.listen(PORT ,()=> {
+console.log(`Server is running on ${PORT}`)
+connectDB();
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,72 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage.js";
+import LoginPage from "./pages/auth/login/LoginPage.js";
+import SignUpPage from "./pages/auth/signup/SignUpPage.js";
+import NotificationPage from "./pages/notification/NotificationPage.js";
+import ProfilePage from "./pages/profile/ProfilePage.js";
+import Sidebar from "./components/common/Sidebar.js";
+import RightPanel from "./components/common/RightPanel.js";
+import { baseUrl } from "../src/constant/url.js";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner.js";
+
+function App() {
+	const { data: authUser, isLoading } = useQuery({
+		// we use queryKey to give a unique name to our query and refer to it later
+		queryKey: ["authUser"],
+		queryFn: async () => {
+			try {
+				const res = await fetch(`${baseUrl}/api/auth/me`,{
+					method: "GET",
+					credentials: "include",
+					headers : {
+						"Content-Type": "application/json",
+					}
+				});
+			
+				const data = await res.json();
+				if (data.error) {
+					return null;
+				}
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				console.log("authUser is here:", data);
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		retry: false,
+	});
+	//console.log (authUser);
+
+	if (isLoading) {
+		return (
+			<div className='h-screen flex justify-center items-center'>
+				<LoadingSpinner size='lg' />
+			</div>
+		);
+	}
+	
+
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{/* Common component, bc it's not wrapped with Routes */}
+			{authUser && <Sidebar />}
+			<Routes>
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+			</Routes>
+			{authUser && <RightPanel />}
+			<Toaster />
+		</div>
+	);
+}
+
+export default App;

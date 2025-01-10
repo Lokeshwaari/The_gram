4334f4587445802bc5da692c10773580a5498730
Lokeshwaari <lokeshwarisai@gmail.com>
2025-01-10 22:28:45 +0530

    modified file

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://the-gram-backend-6lg4.onrender.com"
-export const baseUrl = "http://localhost:5000"
+export const baseUrl = "https://the-gram-backend-6lg4.onrender.com"
+//export const baseUrl = "http://localhost:5000"

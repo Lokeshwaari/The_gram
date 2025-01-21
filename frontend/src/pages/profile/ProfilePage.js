2932a74ce4e3cffc96bce5ef2d62b86e07e2f538
Lokeshwaari R <159430698+Lokeshwaari@users.noreply.github.com>
2025-01-21 16:28:13 +0530

    Update ProfilePage.js

--- frontend/src/pages/profile/ProfilePage.js
+++ frontend/src/pages/profile/ProfilePage.js
@@ -138,7 +138,7 @@ const ProfilePage = () => {
 								<div className='avatar absolute -bottom-16 left-4'>
 									<div className='w-32 rounded-full relative group/avatar'>
 										<img src={profileImg || user?.profileImg ||"/avatar-placeholder.png"} />
-										<div  style="height:35px;" className='absolute height:35px top-5 right-3 p-1 bg-primary rounded-full group-hover/avatar:opacity-100 opacity-0 cursor-pointer'>
+										<div className='absolute height:35px top-5 right-3 p-1 bg-primary rounded-full group-hover/avatar:opacity-100 opacity-0 cursor-pointer'>
 											{isMyProfile && (
 												<MdEdit
 													className='w-4 h-4 text-white'

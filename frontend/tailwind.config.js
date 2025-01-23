71e1c764abf08454cd0b92c5e2a4537937a8124a
Lokeshwaari <lokeshwarisai@gmail.com>
2025-01-23 16:39:38 +0530

    modified file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ import daisyui from "daisyui";
 import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./components/**/*.{js,ts,jsx,tsx}"],
+	content: ["./index.html", "./Src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
